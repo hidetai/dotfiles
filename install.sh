bbcd99b0ac5168e490d141b2066e8b340a2ab6e2
Hide <hidetai.com@gmail.com>
2012-11-23 19:20:10 +0900

    Add an install script for dotfiles

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+################################################################################
+# install.sh
+#
+#   This script creates symolic links from the home directory to dotfiles.
+################################################################################
+
+dotdir=$HOME/dotfiles
+
+## zsh
+ln -ns $dotdir/.zshrc $HOME
+
+## screen
+ln -ns $dotdir/.screenrc $HOME
+
+## emacs
+ln -ns $dotdir/.emacs.d/init.el $HOME/.emacs.d/
