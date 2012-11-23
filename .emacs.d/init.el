03f3dbcca67376286d25973d859c4442a9109b9d
Hide <hidetai.com@gmail.com>
2012-11-24 01:05:11 +0900

    Fix indent

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,11 +14,11 @@
 (defun add-to-load-path (&rest paths)
   (let (path)
     (dolist (path paths paths)
-     (let ((default-directory (expand-file-name
+      (let ((default-directory (expand-file-name
                                (concat user-emacs-directory path))))
-        (add-to-list 'load-path default-directory)
-         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-             (normal-top-level-add-subdirs-to-load-path))))))
+           (add-to-list 'load-path default-directory)
+           (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+               (normal-top-level-add-subdirs-to-load-path))))))
 
 ;; Add directories to load-path
 (add-to-load-path "elisp")
