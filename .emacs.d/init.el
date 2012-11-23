13a9f8d46302692fa75615886677e1e7e578ea44
Hide <hidetai.com@gmail.com>
2012-11-24 02:00:34 +0900

    Add the file list for anything.el search

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -282,6 +282,8 @@
 ;;;----------------------------------------------------------------------------
 
 (when (require 'anything-startup nil t)
+  (setq anything-c-filelist-file-name "/tmp/all.filelist")
+  (setq anything-grep-candidates-fast-directory-regexp "^/tmp")
   (define-key global-map (kbd "C-x b") 'anything-filelist+)
   (define-key global-map (kbd "M-y") 'anything-show-kill-ring))
 
