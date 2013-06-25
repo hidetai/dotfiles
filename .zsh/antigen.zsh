eadb362f7b2e51cdf9cb4fee62673d4c2d48e9dc
Hide <hidetai.com@gmail.com>
2013-06-25 15:37:37 +0900

    Added antigen to change the appearance of zsh.

---
+++ .zsh/antigen.zsh
@@ -0,0 +1,11 @@
+source ~/.zsh/antigen/antigen.zsh
+
+antigen-use oh-my-zsh
+
+antigen-bundle zsh-users/zsh-syntax-highlighting
+antigen-bundle git
+
+# Theme
+antigen-theme robbyrussell
+
+antigen-apply
--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@
 
 dotdir=$HOME/dotfiles
 
+# git submodule
+(cd $dotdir; git submodule init; git submodule update)
+
 ## zsh
 ln -ns $dotdir/.zshrc $HOME
+ln -ns $dotdir/.zsh $HOME.zsh
 
 ## screen
 ln -ns $dotdir/.screenrc $HOME
@@ -17,7 +21,6 @@ ln -ns $dotdir/.screenrc $HOME
 ln -ns $dotdir/.emacs.d/init.el $HOME/.emacs.d/
 
 ## vim
-(cd $dotdir; git submodule init; git submodule update)
 mkdir -p $HOME/.vim/bundle
 ln -ns $dotdir/neobundle.vim $HOME/.vim/bundle/neobundle.vim
 ln -ns $dotdir/.vimrc .vimrc
